e7c4ad7e4dc442d52bbe1ad6daf40deb64ca5a10
bellobambo <bellobambo21@gmail.com>
2024-12-19 09:05:41 +0100

    final chat app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
+const socket = io.connect("http://localhost:5000");
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [inputMessage, setInputMessage] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    setInputMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("sendMessage", { message: inputMessage });
+    setInputMessage("");
+  };
+
+  useEffect(() => {
+    socket.on("new_message", (data) => {
+      setMessage(data.message);
+      console.log(data);
+    });
+  }, [socket]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Chat App</h1>
 
-export default App
+      <form onSubmit={handleSubmit} action="">
+        <input
+          type="text"
+          name="inputMessage"
+          value={inputMessage}
+          onChange={handleChange}
+          placeholder="Enter Message"
+        />
+        <button type="submit">Send</button>
+      </form>
+
+      {message && <h2>{message}</h2>}
+    </div>
+  );
+};
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,29 @@
 require("dotenv").config();
 
-console.log("Server Started...");
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = express();
+
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("sendMessage", (data) => {
+    io.emit("new_message", data);
+    console.log(data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("client disconnected");
+  });
+});
+
+server.listen(port, () => console.log(`server listening on port ${port}`));
